b4a8dceb3fe9d5d0bee12ad006e9609534887dae
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2022-04-25 01:50:07 +0200

    fix warnings about using raw
    also use display name, not userID.

--- src/main/java/com/tbest/simplegraphapplication/HelloController.java
+++ src/main/java/com/tbest/simplegraphapplication/HelloController.java
@@ -3,10 +3,9 @@ package com.tbest.simplegraphapplication;
 import chariot.Client;
 import chariot.model.RatingHistory;
 import javafx.fxml.FXML;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
@@ -16,47 +15,41 @@ import java.util.function.Consumer;
 
 public class HelloController {
     @FXML
-    private ScatterChart chart;
+    private ScatterChart<String, Integer> chart;
 
     @FXML
     private TextField username;
 
     @FXML
-    private Spinner minRatingDays;
+    private Spinner<?> minRatingDays;
 
 
     @FXML
     protected void onSubmitButtonClick() {
-        System.out.println(minRatingDays.getValue());
-        minRatingDays.commitValue();
-        System.out.println(minRatingDays.getValue());
-
-        ScatterChart(chart, username.getText().toLowerCase());
+        // Username to lower case, since with the lichess api the ID is always lower case
+        UpdateScatterChart(chart, username.getText().toLowerCase());
     }
 
-    private ScatterChart<String, Number> ScatterChart(ScatterChart scatterChart, String user) {
+    private void UpdateScatterChart(ScatterChart<String, Integer> scatterChart, String user) {
         var client = Client.basic();
+        if (client.users().byId(user).isPresent() == false) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "User not found");
+            alert.show();
+            return;
+        }
+        String displayName = client.users().byId(user).get().username();
         var Result = client.users().ratingHistoryById(user);
-
-        //Defining the axes
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Time");
-
-        NumberAxis yAxis = new NumberAxis(600, 3000, 100);
-        yAxis.setLabel("Points");
-
 
         Consumer<RatingHistory> addPoint = RatingHistory -> {
             //Prepare XYChart.Series objects by setting data
-            XYChart.Series series = new XYChart.Series();
-            series.setName(username.getText() + RatingHistory.name());
+            XYChart.Series<String, Integer> series = new XYChart.Series<>();
+            series.setName(displayName + " " + RatingHistory.name());
             for (chariot.model.RatingHistory.DateResult dateResult : RatingHistory.results()) {
                 //    System.out.println(dateResult.date() + " " + dateResult.points());
-                series.getData().add(new XYChart.Data(dateResult.date().toString(), dateResult.points()));
-
+                series.getData().add(new XYChart.Data<>(dateResult.date().toString(), dateResult.points()));
             }
-            //Only include categorises with minRatingDays or more points
 
+            //Only include categorises with minRatingDays or more points
             System.out.println(RatingHistory.name() + " : " + series.getData().size());
             if (series.getData().size() > (Integer) minRatingDays.getValue()) {
                 //Setting the data to scatter chart
@@ -64,19 +57,22 @@ public class HelloController {
             }
         };
         Result.stream().forEach(addPoint);
+
         //Styling the chart
-        scatterChart.setTitle(user.toUpperCase());
-        scatterChart.autosize();
-        scatterChart.setVerticalGridLinesVisible(false);
+        if (scatterChart.getTitle() != null) {
+            scatterChart.setTitle(scatterChart.getTitle() + " " + displayName);
+        } else {
+            scatterChart.setTitle(displayName);
+        }
         final DropShadow shadow = new DropShadow();
         shadow.setOffsetX(2);
         shadow.setColor(Color.GREY);
         scatterChart.setEffect(shadow);
-
-        return scatterChart;
     }
 
     @FXML
     private void onClearButtonClick() {
+        chart.getData().removeAll();
+        System.out.println("clear clicked");
     }
 }
