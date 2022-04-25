88d2cd2da08386ae378ea9805cf4a34a04e9cf18
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2022-04-25 13:03:54 +0200

    Square window and undecorated

--- src/main/java/com/tbest/simplegraphapplication/HelloApplication.java
+++ src/main/java/com/tbest/simplegraphapplication/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -16,9 +17,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), Screen.getPrimary().getBounds().getWidth(),
-                Screen.getPrimary().getBounds().getHeight());
+        //Square window
+        double size = Math.min(Screen.getPrimary().getBounds().getWidth(), Screen.getPrimary().getBounds().getHeight());
+        Scene scene = new Scene(fxmlLoader.load(), size, size);
+
         stage.setTitle("Lichess Rating Graph");
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
