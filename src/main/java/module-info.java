dd791c50bbdb40777e37eb0b242bf87fded7d1ac
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2022-04-27 14:19:45 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,11 @@ module com.tbest.simplegraphapplication {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires chariot;
+    requires java.desktop;
 
     opens com.tbest.simplegraphapplication to javafx.fxml;
     exports com.tbest.simplegraphapplication;
