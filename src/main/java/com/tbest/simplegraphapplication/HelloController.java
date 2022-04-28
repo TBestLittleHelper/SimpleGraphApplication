f44470bb6a7f3e4097f738a7acf65a41a402fdc9
TBestLittleHelper <TBestLittleHelper@users.noreply.github.com>
2022-04-28 17:36:00 +0200

    Change spinner to select a variant

--- src/main/java/com/tbest/simplegraphapplication/HelloController.java
+++ src/main/java/com/tbest/simplegraphapplication/HelloController.java
@@ -2,6 +2,8 @@ package com.tbest.simplegraphapplication;
 
 import chariot.Client;
 import chariot.model.RatingHistory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
@@ -9,6 +11,7 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 import javafx.util.StringConverter;
 
@@ -27,9 +30,22 @@ public class HelloController {
     private TextField username;
 
     @FXML
-    private Spinner<?> minRatingDays;
+    private Spinner<String> variantSpinner;
+
+    private int numberOfUsers;
 
     public void initialize() {
+        numberOfUsers = 0;
+        ObservableList<String> variantsList = FXCollections.observableArrayList(//
+                "Bullet", "Blitz", "Rapid", "Classical", //
+                "Correspondence", "Crazyhouse", "Chess960", "King of the Hill", //
+                "Three-check", "Antichess", "Atomic", "Horde", "Racing Kings", "Puzzles");
+        SpinnerValueFactory.ListSpinnerValueFactory<String> valueFactory = new SpinnerValueFactory.ListSpinnerValueFactory<String>(variantsList);
+        valueFactory.setValue("Blitz");
+        valueFactory.wrapAroundProperty();
+        variantSpinner.setValueFactory(valueFactory);
+
+
         xAxis.setTickLabelFormatter(new StringConverter<>() {
             @Override
             public String toString(Number number) {
@@ -60,10 +76,19 @@ public class HelloController {
         System.out.println("Requesting " + user);
         final var client = Client.basic();
         if (!client.users().byId(user).isPresent()) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "User not found");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, user + " not found");
             alert.show();
             return;
         }
+        if (client.users().byId(user).get().disabled()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, user + " is disabled");
+            alert.show();
+            return;
+        }
+
+        //We found the user, so increment
+        numberOfUsers++;
+
         final String displayName = client.users().byId(user).get().username();
         final var Result = client.users().ratingHistoryById(user);
 
@@ -75,50 +100,48 @@ public class HelloController {
                 series.getData().add(new XYChart.Data<>(dateResult.date().toEpochSecond(LocalTime.MIN, ZoneOffset.UTC), dateResult.points()));
             }
 
-            //Only include categorises with minRatingDays or more points
-            if (series.getData().size() > (Integer) minRatingDays.getValue()) {
-                System.out.println(RatingHistory.name() + " : " + series.getData().size());
-
+            //Only include the selected variant
+            if (RatingHistory.name().equals(variantSpinner.getValue())) {
 
                 //Setting the data to scatter chart
                 lineChart.getData().add(series);
 
-
                 //Set color for series, nodes and legend.
-                final String color = getColor(RatingHistory.name());
-                System.out.println(color);
+                final String variantColor = getVariantColor(RatingHistory.name());
+                final String colorPlayer = getPlayerColor();
 
+                System.out.println(RatingHistory.name() + " : " + series.getData().size() + variantSpinner.getValue());
+                System.out.println(displayName + " " + colorPlayer + " " + variantColor);
 
-                //Style nodes
+                //Style line nodes
                 int index = 0;
                 while (index < series.getData().size()) {
                     XYChart.Data<Long, Integer> dataPoint = series.getData().get(index);
                     Node lineSymbol = dataPoint.getNode().lookup(".chart-line-symbol");
-                    lineSymbol.setStyle("-fx-background-radius: 10px; -fx-background-color: " + color);
-//-fx-shape: M40,60 C42,48 44,30 25,32 Z;
+                    lineSymbol.setStyle("-fx-background-radius: 1px;" +
+                            " -fx-background-color: " + colorPlayer);
                     index++;
                 }
                 //Color lines
-                StringBuilder seriesStyleString = new StringBuilder("-fx-stroke-width: 3; -fx-stroke: " + color);
+                StringBuilder seriesStyleString = new StringBuilder("-fx-stroke-width: 1; -fx-stroke: " + variantColor);
                 series.getNode().setStyle(seriesStyleString.toString());
 
                 //Color chart legend
                 for (Node n : chart.getChildrenUnmodifiable()) {
-                    if (n.getClass().getSimpleName() == "Legend") {
-                        // n.setStyle("-fx-background-color: #0000ff, " + color);
+                    if (n.getClass().getSimpleName().equals("Legend")) {
+                        n.setStyle("-fx-background-color:" + colorPlayer);
+                        //-fx-create-symbols: M 100 100L 300 100L 200 300Z;
                     }
-
                 }
-
             }
         };
         Result.stream().forEach(addPoint);
 
         //Update the chart title
         if (lineChart.getTitle() != null) {
-            lineChart.setTitle(lineChart.getTitle() + ", " + displayName);
+            lineChart.setTitle(lineChart.getTitle() + ", " + displayName + " " + variantSpinner.getValue());
         } else {
-            lineChart.setTitle(displayName);
+            lineChart.setTitle(displayName + " " + variantSpinner.getValue());
         }
     }
 
@@ -126,6 +149,7 @@ public class HelloController {
     private void onClearButtonClick() {
         chart.getData().clear();
         chart.setTitle(null);
+        numberOfUsers = 0;
     }
 
     @FXML
@@ -133,7 +157,7 @@ public class HelloController {
         System.exit(0);
     }
 
-    private String getColor(String variant) {
+    private String getVariantColor(String variant) {
         return switch (variant) {
             case "Bullet" -> "RED";
             case "Blitz" -> "BLUE";
@@ -146,6 +170,18 @@ public class HelloController {
             case "Puzzles" -> "LIGHTGOLDENRODYELLOW";
 
             default -> "GRAY";
+        };
+    }
+
+    private String getPlayerColor() {
+        return switch (numberOfUsers) {
+            case 1 -> "LAWNGREEN";
+            case 2 -> "LIGHTBLUE";
+            case 3 -> "DARKMAGENTA";
+            case 4 -> "LIGHTSALMON";
+            case 5 -> "LIGHTPINK";
+
+            default -> "GREY";
         };
     }
 }
